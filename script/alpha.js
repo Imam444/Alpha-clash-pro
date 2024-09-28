cfe402e84e84f77718f15db038cc5e31f78e3940
Imam444 <mdimamhossain565528@gmail.com>
2024-09-28 22:54:37 +0600

    added button background color

--- script/alpha.js
+++ script/alpha.js
@@ -2,11 +2,18 @@
 
 function continueGame (){
     const alphabet =getARandomAlphabet();
-}
+    console.log('your random alphabet',alphabet)
 
+    const currentAlphabetElement =document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText =alphabet;
+
+    setBackgroundColorByID(alphabet);
+
+}
 
 function play(){
    hideElementById('home')
    showElementById('play-ground')
    continueGame()
 }
+
--- script/utility.js
+++ script/utility.js
@@ -11,6 +11,12 @@ function showElementById (elementId){
 
 }
 
+function setBackgroundColorByID(elementId){
+    const element =document.getElementById(elementId)
+    element.classList.add('bg-orange-400');
+
+}
+
 function getARandomAlphabet (){
     const alphabetString ='a b c d e f g h i j k l m n p q r t s u v w x y z';
     const alphabets = alphabetString.split(' ');
